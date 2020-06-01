0a8e1851dc7372edd49b3fce037e4ae3c46270d8
Diego Saldao <diego@newcom-lcs.com>
2020-06-01 11:45:40 -0300

    Agregar calculo de cotización

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-function calcularTotal(cantidad, plazo){
+export function calcularTotal(cantidad, plazo) {
     //cantidades
     // hasta 1000 = 25%
     //hasta 5000 = 20%
@@ -6,11 +6,11 @@ function calcularTotal(cantidad, plazo){
     //+10000 = 10%
 
     let totalCantidad;
-    if(cantidad <= 1000){
+    if (cantidad <= 1000) {
         totalCantidad = cantidad * .25;
-    } else if(cantidad> 1000 && cantidad < 5000){
+    } else if (cantidad > 1000 && cantidad < 5000) {
         totalCantidad = cantidad * .20;
-    } else if(cantidad > 5000 && cantidad < 10000){
+    } else if (cantidad > 5000 && cantidad < 10000) {
         totalCantidad = cantidad * .15;
     } else {
         totalCantidad = cantidad * .10
@@ -22,4 +22,25 @@ function calcularTotal(cantidad, plazo){
     //12 = 15%
     //24 = 20%
     console.log(totalCantidad);
+
+    let totalPlazo = 0;
+    switch (plazo) {
+        case 3:
+            totalPlazo = cantidad * .05;
+            break;
+        case 6:
+            totalPlazo = cantidad * .10;
+            break;
+        case 9:
+            totalPlazo = cantidad * .15;
+            break;
+        case 12:
+            totalPlazo = cantidad * .20;
+            break;
+        case 24:
+            totalPlazo = cantidad * .25;
+            break;
+    }
+
+    return totalCantidad + totalCantidad + cantidad;
 }
